0820c1e3d9df389e090eec0e0c16a28153b02e81
고태경 <202244042@itc.ac.kr>
2024-03-07 18:15:27 +0900

    할인 정책 구현

--- src/main/java/com/whydev/saysno/discount/policy/DiscountPolicy.java
+++ src/main/java/com/whydev/saysno/discount/policy/DiscountPolicy.java
@@ -3,6 +3,8 @@ package com.whydev.saysno.discount.policy;
 import com.whydev.saysno.discount.DiscountRequest;
 
 public interface DiscountPolicy {
+    // 할인 정책 정의 인터페이스
+
     // 할인 정책 확인
     boolean isSatisfiedBy(DiscountRequest discountDTO);
 
